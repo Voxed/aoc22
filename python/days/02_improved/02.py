e8402050c097607a5dd26ee797abfa62f871117d
Voxed <voxedg@gmail.com>
2022-12-02 07:02:33 +0100

    "improved" unreadable solution

---
+++ python/days/02_improved/02.py
@@ -0,0 +1,9 @@
+# Part1: 00:07:17  
+# Part2: 00:12:29
+
+def score(i):
+    return (3 if (i[0]-1)%3 != i[1] else 0) + (3 if (i[0]+1)%3 == i[1] else 0) + i[1]+1
+
+def start(inp):
+    cmds = [(lambda g: [ord(g[0])-65, ord(g[1])-88])(f.split(' ')) for f in inp.split('\n')]
+    print(sum(map(score, cmds)), sum(map(score, map(lambda x: [x[0], (x[0] + x[1] - 1)%3], cmds))))
