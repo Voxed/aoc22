0cd813d2e1f9e46dd9372cb0b2853a9d72faa49d
Voxed <voxedg@gmail.com>
2022-12-06 06:50:45 +0100

    sliding_window_view to the rescue!

--- python/days/06_improved/06.py
+++ python/days/06_improved/06.py
@@ -5,6 +5,7 @@ import numpy as np
 from numpy.lib import stride_tricks as st
 import utils as u
 
+
 def start(inp, _):
-    print((u.super_map([np.unique, len, lambda x: x < 4], st.sliding_window_view(list(inp), 4))).index(False)+4)
-    print((u.super_map([np.unique, len, lambda x: x < 14], st.sliding_window_view(list(inp), 14))).index(False)+14)
+    print((u.super_map([np.unique, len, lambda x: x < 4], st.sliding_window_view(list(inp), 4))).index(False)+4,
+          (u.super_map([np.unique, len, lambda x: x < 14], st.sliding_window_view(list(inp), 14))).index(False)+14)
